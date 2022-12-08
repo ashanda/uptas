c34c15fd59dfb599c87e6ae60c4cc526abc94cfa
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-08 16:37:03 +0530

    blog and why choose us pages

---
+++ wp-content/themes/uptas/page-blog.php
@@ -0,0 +1,220 @@
+<?php
+/*
+Template Name: Blog Page
+*/
+get_header();  ?>
+<!-- Banner Start -->
+
+<div class="sample-banner" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/banner.jpg');">
+  <div class="container text-center">
+    <div class="banner-maintext">
+      <h2 class="main-heading">Uptas Blog</h2>
+    </div>
+    <div class="d-flex flex-md-row flex-wrap justify-content-center">
+      <div class="page-links">
+        <a href="Home" class="pa-links">Home/</a>
+        <a href="Home" class="pa-links">Chemical Reactions</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Wrapper End -->
+
+<!--Body Content Wrapper Start-->
+<section class="section-card">
+  <div class="container p-0">
+    <div class="cards">
+      <div class="row mt-4 pl-4">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="card new-card">
+            <div class="image-area text-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/blog_teach.jpg');">
+              <div class="date-time">
+                <p class="date-number">22</p>
+                <p class="month-name">JUN</p>
+              </div>
+              <div class="exper">
+                <p class="experiment text-center">FUN EXPERIMENTS</p>
+              </div>
+
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title">Loream ipsum is simply dummy</h5>
+
+              <div class="other-links d-flex flex-md-row flex-wrap justify-content-center">
+                <p class="post-by pr-2">post by</p>
+                <p class="circle"><a href=""><i class="fa fa-solid fa-circle"></i></a></p>
+                <p class="chathuranga">d.chathuranga</p>
+                <p class="comment"><a href=""><i class="fa fa-light fa-message"></i></a></p>
+                <p class="share"><a href=""><i class="fa fa-solid fa-share-nodes"></i></a></p>
+              </div>
+              <div class="card-description">
+                <p class="card-text">but also the leap into electronic typesetting, remaining
+                  essentially unchanged.</p>
+                <a href="#" class="des">continue reading</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="card new-card">
+            <div class="image-area text-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/blog_teach.jpg');">
+              <div class="date-time">
+                <p class="date-number">22</p>
+                <p class="month-name">JUN</p>
+              </div>
+              <div class="exper">
+                <p class="experiment text-center">FUN EXPERIMENTS</p>
+              </div>
+
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title">Loream ipsum is simply dummy</h5>
+
+              <div class="other-links d-flex flex-md-row flex-wrap justify-content-center">
+                <p class="post-by pr-2">post by</p>
+                <p class="circle"><a href=""><i class="fa fa-solid fa-circle"></i></a></p>
+                <p class="chathuranga">d.chathuranga</p>
+                <p class="comment"><a href=""><i class="fa fa-light fa-message"></i></a></p>
+                <p class="share"><a href=""><i class="fa fa-solid fa-share-nodes"></i></a></p>
+              </div>
+              <div class="card-description">
+                <p class="card-text">but also the leap into electronic typesetting, remaining
+                  essentially unchanged.</p>
+                <a href="#" class="des">continue reading</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="card new-card">
+            <div class="image-area text-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/blog_teach.jpg');">
+              <div class="date-time">
+                <p class="date-number">22</p>
+                <p class="month-name">JUN</p>
+              </div>
+              <div class="exper">
+                <p class="experiment text-center">FUN EXPERIMENTS</p>
+              </div>
+
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title">Loream ipsum is simply dummy</h5>
+
+              <div class="other-links d-flex flex-md-row flex-wrap justify-content-center">
+                <p class="post-by pr-2">post by</p>
+                <p class="circle"><a href=""><i class="fa fa-solid fa-circle"></i></a></p>
+                <p class="chathuranga">d.chathuranga</p>
+                <p class="comment"><a href=""><i class="fa fa-light fa-message"></i></a></p>
+                <p class="share"><a href=""><i class="fa fa-solid fa-share-nodes"></i></a></p>
+              </div>
+              <div class="card-description">
+                <p class="card-text">but also the leap into electronic typesetting, remaining
+                  essentially unchanged.</p>
+                <a href="#" class="des">continue reading</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-4 pl-4 mb-4">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="card">
+            <div class="image-area text-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/blog_teach.jpg');">
+              <div class="date-time">
+                <p class="date-number">22</p>
+                <p class="month-name">JUN</p>
+              </div>
+              <div class="exper">
+                <p class="experiment text-center">FUN EXPERIMENTS</p>
+              </div>
+
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title">Loream ipsum is simply dummy</h5>
+
+              <div class="other-links d-flex flex-md-row flex-wrap justify-content-center">
+                <p class="post-by pr-2">post by</p>
+                <p class="circle"><a href=""><i class="fa fa-solid fa-circle"></i></a></p>
+                <p class="chathuranga">d.chathuranga</p>
+                <p class="comment"><a href=""><i class="fa fa-light fa-message"></i></a></p>
+                <p class="share"><a href=""><i class="fa fa-solid fa-share-nodes"></i></a></p>
+              </div>
+              <div class="card-description">
+                <p class="card-text">but also the leap into electronic typesetting, remaining
+                  essentially unchanged.</p>
+                <a href="#" class="des">continue reading</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="card">
+            <div class="image-area text-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/blog_teach.jpg');">
+              <div class="date-time">
+                <p class="date-number">22</p>
+                <p class="month-name">JUN</p>
+              </div>
+              <div class="exper">
+                <p class="experiment text-center">FUN EXPERIMENTS</p>
+              </div>
+
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title">Loream ipsum is simply dummy</h5>
+
+              <div class="other-links d-flex flex-md-row flex-wrap justify-content-center">
+                <p class="post-by pr-2">post by</p>
+                <p class="circle"><a href=""><i class="fa fa-solid fa-circle"></i></a></p>
+                <p class="chathuranga">d.chathuranga</p>
+                <p class="comment"><a href=""><i class="fa fa-light fa-message"></i></a></p>
+                <p class="share"><a href=""><i class="fa fa-solid fa-share-nodes"></i></a></p>
+              </div>
+              <div class="card-description">
+                <p class="card-text">but also the leap into electronic typesetting, remaining
+                  essentially unchanged.</p>
+                <a href="#" class="des">continue reading</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="card">
+            <div class="image-area text-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/blog_teach.jpg');">
+              <div class="date-time">
+                <p class="date-number">22</p>
+                <p class="month-name">JUN</p>
+              </div>
+              <div class="exper">
+                <p class="experiment text-center">FUN EXPERIMENTS</p>
+              </div>
+
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title">Loream ipsum is simply dummy</h5>
+
+              <div class="other-links d-flex flex-md-row flex-wrap justify-content-center">
+                <p class="post-by pr-2">post by</p>
+                <p class="circle"><a href=""><i class="fa fa-solid fa-circle"></i></a></p>
+                <p class="chathuranga">d.chathuranga</p>
+                <p class="comment"><a href=""><i class="fa fa-light fa-message"></i></a></p>
+                <p class="share"><a href=""><i class="fa fa-solid fa-share-nodes"></i></a></p>
+              </div>
+              <div class="card-description">
+                <p class="card-text">but also the leap into electronic typesetting, remaining
+                  essentially unchanged.</p>
+                <a href="#" class="des">continue reading</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/uptas/page-why_choose.php
@@ -0,0 +1,171 @@
+<?php
+/*
+Template Name: Why Choose Us
+*/
+get_header();  ?>
+<!-- Banner Start -->
+
+<div class="wc_sec1 section">
+  <div class="container">
+    <div class="sec_content">
+      <div class="row">
+        <div class="col-sm-6 col-md-3">
+          <div class="sing_sec">
+            <span class="blue">
+              Some Words About Us
+            </span>
+            <h1 class="mt-3">WHY YOU SHOULD CHOOSE US</h1>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+          <div class="sing_sec">
+            <span class="blue">
+              Ai Base learning
+            </span>
+            <p class="mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam commodi autem obcaecati, possimus in dolore consectetur</p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+          <div class="sing_sec">
+            <span class="blue">
+              Some Words About Us
+            </span>
+            <p class="mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam commodi autem obcaecati, possimus in dolore consectetur</p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+          <div class="sing_sec">
+            <span class="blue">
+              Some Words About Us
+            </span>
+            <p class="mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam commodi autem obcaecati, possimus in dolore consectetur</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="wc_sec2 mt-2 section">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="wc_sec2_left_img">
+          <img src="<?php echo get_template_directory_uri(); ?>/inc/img/teach.jpg" alt="" class="img-fluid">
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="wc_sec2_right_capt">
+          <span class="blue fw-bold">
+            Online and onsite classes
+          </span>
+          <h3 class="my-3 fw-bold">
+            About Our Onsite Classes
+          </h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis in accusantium quaerat! Deleniti debitis nulla, vitae dignissimos minus numquam esse velit asperiores modi, doloribus veritatis! Incidunt nesciunt molestiae ut eum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse molestiae voluptates facilis rem nemo, quo quasi tempora earum ab eligendi cum possimus ipsum nostrum, quia placeat at numquam eius in?</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque suscipit natus et repudiandae animi voluptate cumque laborum? Et eveniet quasi rerum laudantium, sint animi. Ducimus deserunt culpa quod veritatis incidunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed at enim quaerat labore hic officiis quis molestias. Maxime praesentium minima totam aut magni voluptatum suscipit, accusantium necessitatibus cupiditate, illum soluta.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-1 mt-lg-5">
+      <div class="col-sm-6">
+        <div class="wc_sec2_right_capt">
+          <span class="blue fw-bold">
+            Artificial Interligence
+          </span>
+          <h3 class="my-3 fw-bold">
+            AI Base Learning
+          </h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis in accusantium quaerat! Deleniti debitis nulla, vitae dignissimos minus numquam esse velit asperiores modi, doloribus veritatis! Incidunt nesciunt molestiae ut eum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse molestiae voluptates facilis rem nemo, quo quasi tempora earum ab eligendi cum possimus ipsum nostrum, quia placeat at numquam eius in?</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque suscipit natus et repudiandae animi voluptate cumque laborum? Et eveniet quasi rerum laudantium, sint animi. Ducimus deserunt culpa quod veritatis incidunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed at enim quaerat labore hic officiis quis molestias. Maxime praesentium minima totam aut magni voluptatum suscipit, accusantium necessitatibus cupiditate, illum soluta.</p>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="wc_sec2_left_img">
+          <img src="<?php echo get_template_directory_uri(); ?>/inc/img/ai.jpg" alt="" class="img-fluid">
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="wc_mid_banner section" style="background-image: linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)) , url('<?php echo get_template_directory_uri(); ?>/inc/img/banner.jpg')">
+  <div class="container">
+    <table class="out_table">
+      <tr>
+        <td class="out_table_td text-center text-sm-start">
+          <span class="blue">Ai BAse LEarning</span>
+          <h3 class="my-3">
+            Ai BAse LEarning
+          </h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, </p>
+        </td>
+        <td class="out_table_td">
+          <table class="in_table mt-5 mt-md-0">
+            <tr class="border-bottom">
+              <td class="border-right">
+                <h1>2012</h1>
+                <p>Founding Year</p>
+              </td>
+              <td>
+                <h1>2012</h1>
+                <p>Founding Year</p>
+              </td>
+            </tr>
+            <tr class="border-bottom">
+              <td class="border-right">
+                <h1 class="mt-2">99%</h1>
+                <p>Pass Rate</p>
+              </td>
+              <td>
+                <h1 class="mt-2">2012</h1>
+                <p>Founding Year</p>
+              </td>
+            </tr>
+            <tr>
+              <td class="border-right">
+                <h1 class="mt-2">2+</h1>
+                <p>facilities</p>
+              </td>
+              <td>
+                <h1 class="mt-2">750+</h1>
+                <p>Students</p>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </div>
+</div>
+<div class="wc_bot_sec section mb-2 mb-sm-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="video_card">
+          <div class="card">
+            <iframe src="https://www.youtube.com/embed/pWahNIMRxR0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="card-body">
+              <h5 class="card-title">Company Histry and Facts</h5>
+              <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio accusantium deserunt porro cupiditate reprehenderit repellendus velit eaque, quasi possimus hic dicta nisi quam culpa inventore magnam magni veritatis. Asperiores, neque.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="video_card">
+          <div class="card">
+            <iframe src="https://www.youtube.com/embed/pWahNIMRxR0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="card-body">
+              <h5 class="card-title">Student Stories</h5>
+              <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio accusantium deserunt porro cupiditate reprehenderit repellendus velit eaque, quasi possimus hic dicta nisi quam culpa inventore magnam magni veritatis. Asperiores, neque.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
