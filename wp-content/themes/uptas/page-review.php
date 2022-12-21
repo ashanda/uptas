c8659e2fbe3d7d0516f53f5cc6bab33ffa4d7892
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-21 19:04:22 +0530

    REVIEW PAGE

---
+++ wp-content/themes/uptas/page-review.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Review
+*/
+get_header();  ?>
+
+<!-- Banner Start -->
+<?php get_template_part('inner-banner'); ?>
+<!-- Banner Wrapper End -->
+<!-- we are secion -->
+<div class="review_top py-4 text-center">
+    <h2>WHY WE ARE BEST AT TEACHING</h2>
+</div>
+<div class="review section">
+    <div class="container">
+
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
