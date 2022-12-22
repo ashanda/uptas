9b7fb9de8e6693c256910d409e51fbaa74faa966
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-22 17:57:10 +0530

    done

--- wp-content/themes/uptas/page-review.php
+++ wp-content/themes/uptas/page-review.php
@@ -11,6 +11,9 @@ get_header();  ?>
 <div class="review_top py-4 text-center">
     <h2>WHY WE ARE BEST AT TEACHING</h2>
 </div>
+<div class="sign_up">
+    <a href="<?php the_field('sign_up_button'); ?>" class="btn btn-primary">Sign Up</a>
+</div>
 <div class="review section">
     <div class="container">
         <?php echo do_shortcode('[grw id="417"]'); ?>
