5ca2356dae55daf192338692e3e635c7a2ff9c2d
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-30 13:04:19 +0530

    test

--- wp-content/themes/uptas/page-review.php
+++ wp-content/themes/uptas/page-review.php
@@ -21,33 +21,42 @@ get_header();  ?>
         <?php echo do_shortcode('[grw id="417"]'); ?>
         <!-- new custom fields -->
         <div class="revw_sec p-5">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="img_sec">
-                        <img class="img-fluid m-3" src="https://uptas.sg/wp-content/uploads/2020/01/XINCI-TJC.jpg" alt="">
-                    </div>
-                </div>
-                <div class="col-md-8">
-                    <div class="rew_sec">
-                        <div class="nme">
-                            <p class="text-secondary fw-bold">XINCI, TJC</p>
-                            <div class="row">
-                                <div class="col-10">
-                                    <p>“You believed in me and helped me to understand Chemistry much, much better”</p>
-                                </div>
-                                <div class="col-2 text-end">
-                                    <a class="btn btn-sm text-small" href=""><img src="<?php echo get_template_directory_uri(); ?>/inc/img/sign-up-button-50.png" alt=""></a>
+            <?php if (have_rows('google_reviews')) : ?>
+                <?php while (have_rows('google_reviews')) : the_row();  ?>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="img_sec">
+                                <img class="img-fluid m-3" src="<?php the_sub_field('image'); ?>" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="rew_sec">
+                                <div class="nme">
+                                    <p class="text-secondary fw-bold mb-1"><?php the_sub_field('name'); ?></p>
+                                    <div class="row">
+                                        <div class="col-10">
+                                            <p>“<?php the_sub_field('caption'); ?>”</p>
+                                        </div>
+                                        <div class="col-2 text-end">
+                                            <a class="btn btn-sm text-small" href=""><img src="<?php echo get_template_directory_uri(); ?>/inc/img/sign-up-button-50.png" alt=""></a>
+                                        </div>
+                                    </div>
+                                    <p class="desc my-3 p-3">
+                                        <?php the_sub_field('description'); ?>
+                                    </p>
                                 </div>
                             </div>
-                            <p class="desc my-3 p-3">
-                                Patient teachers are the BEST but the free food and drinks are pretty good loo hehe! I used to hate chemistry when I was in Integrated Programme (IP) Year 3 & 4. My chemistry tutor in IP 4 even advised me not to touch chemistry at JC level. You were super patient and helpful throughout my 2 years in Uptas. More importantly, you believed in me. You helped me to understand Chemistry much, much better.
-
-                                You were clear and enthusiastic in your teaching and explained it in more simplistic ways to aid understanding. I would recommend all my friends who need help in Chemistry and don’t like it to join ? Throwing back to the times when you could repeat over and over again and slowly teach me concepts without getting upset, I am extremely thankful that I got to know you. I will never forget the extra lessons to help boost our chemistry grade. Thank you Ms Tham, you are really like a good friend which makes lesson fun and comfortable.
-                            </p>
                         </div>
                     </div>
-                </div>
-            </div>
+            <?php
+                endwhile;
+
+
+            else :
+
+            endif;
+            ?>
+
         </div>
         <!-- new custom fields -->
     </div>
