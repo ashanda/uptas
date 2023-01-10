9159a9672e02f341ff6dd90e5976dbd38f3b7d23
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-01-10 15:12:52 +0530

    tour page

--- wp-content/themes/uptas/page-tour.php
+++ wp-content/themes/uptas/page-tour.php
@@ -1,74 +1,130 @@
 <?php
 /*
-Template Name: Review
+Template Name: Tour
 */
 get_header();  ?>
 
-<!-- Banner Start -->
-<?php
-if (wp_get_attachment_url(get_post_thumbnail_id($post->ID))) {
-    $bannerUrl = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-} else {
-    $bannerUrl = get_field('inner_banner');
-}
-?>
-<div class="default_banner" style="background-image: url(<?php echo $bannerUrl; ?>);">
-    <?php get_template_part('inner-banner'); ?>
-</div>
-<!-- Banner Wrapper End -->
 <!-- we are secion -->
-<div class="review_top py-4 text-center">
-    <h2>WHY WE ARE BEST AT TEACHING</h2>
-</div>
-<!-- <div class="container">
-    <div class="sign_up">
-        <a href="<?php the_field('sign_up_button'); ?>" target="_blank" class="btn btn-primary">Sign Up</a>
-    </div>
-</div> -->
-<div class="review section">
+<div class="tour_sec">
     <div class="container">
-        <!-- new custom fields -->
-        <div class="revw_sec p-3 p-sm-5">
-            <?php if (have_rows('google_reviews')) : ?>
-                <?php while (have_rows('google_reviews')) : the_row();  ?>
-                    <div class="row pb-4 mb-4 border-bottom">
-                        <div class="col-md-4">
-                            <div class="img_sec p-4">
-                                <img class="img-fluid m-3 w-100" src="<?php the_sub_field('image'); ?>" alt="">
-                            </div>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="rew_sec">
-                                <div class="nme mt-5 md-md-0">
-                                    <p class="text-secondary fw-bold mb-1"><?php the_sub_field('name'); ?></p>
-                                    <div class="row">
-                                        <div class="col-10">
-                                            <p>“<?php the_sub_field('caption'); ?>”</p>
-                                        </div>
-                                        <div class="col-2 text-end p-0">
-                                            <a class="btn btn-sm text-small" href="<?php echo get_home_url(); ?>/contact-us/#contact_sec"><img src="<?php echo get_template_directory_uri(); ?>/inc/img/sign-up-button-50.png" alt=""></a>
-                                        </div>
-                                    </div>
-                                    <div class="desc my-3 p-3">
-                                        <?php the_sub_field('description'); ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div class="frm">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="img_sec py-4">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/chemistry-tuition-improvement.jpg" alt="">
                     </div>
-            <?php
-                endwhile;
-
-
-            else :
-
-            endif;
-            ?>
-
+                </div>
+                <div class="col-md-6">
+                    <?php echo do_shortcode('[contact-form-7 id="130" title="Get a Free Session!"]'); ?>
+                </div>
+            </div>
         </div>
-        <!-- new custom fields -->
+        <div class="mid_sec">
+            <div class="row">
+                <div class="col-md-8 bg-purple">
+                    <div class="p-4">
+                        <h2 class="text-light mb-3 fw-bold">Be Coached by the Best</h2>
+                        <h5 class="text-light fw-bold">
+                            Tutor Factor- With more than 10 years of teaching experience in
+                            both MOE schools and University, Dr Aw and Miss Tham had
+                            worked with a wide spectrum of students, ranging from
+                            those who are gifted in Chemistry to those who hated
+                            Chemistry. Dr Aw explain concepts to students in a simplified
+                            manner. Armed with years of experience and familiarity with
+                            common gaps faced by students, he is able to give accurate
+                            feedback to students and spot on their difficulties. In this
+                            manner, every class is highly efficient and productive for the
+                            students.
+</h5>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/tour-001.jpg" alt="">
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-4">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/tour-002.jpg" alt="">
+                </div>
+                <div class="col-md-8 bg-purple">
+                    <div class="p-4">
+                        <h2 class="text-light mb-3 fw-bold">Study chemistry sometimes can
+                            be a MAZE</h2>
+                        <h5 class="text-light fw-bold">Dr Aw simplify all chapters of the syllabus into cookie,
+                            bite size. With crystal clear explanations, research proven
+                            teaching methods, top-notched teaching materials,
+                            learning Chemistry had never been easier</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 mt-4">
+                    <div class="tour_card p-3">
+                        <img class="img-fluid w-100" src="<?php echo get_template_directory_uri(); ?>/inc/img/tour-003.jpg" alt="">
+                        <h5 class="py-2 fw-bold">Topic: Chemical Bonding</h5>
+                        <p>Every chapter are sorted to concept levels. Through many
+                            years of interactions with students, we have discovered that
+                            the secret to doing well in exam lies in getting the key
+                            concept of the chapter correct.
+                            <br><br>
+                            An analogy will be if focuses are doors, then key concept
+                            will be the keys to the doors where students will see the big
+                            picture of the content. In this manner, we will further break
+                            down each focus point to concept keys where
+                            misconception will be clarified and the door to distinctions
+                            will be opened.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-6 mt-4">
+                    <div class="tour_card p-3">
+                        <img class="img-fluid w-100" src="<?php echo get_template_directory_uri(); ?>/inc/img/tour-004.jpg" alt="">
+                        <h5 class="py-2 fw-bold">Student Attention Span</h5>
+                        <p>In Uptas Learning Hub, we recognise that students
+                            attention span is short and follows a general trend as
+                            indicated in graph 1. Therefore, the lessons are planned
+                            carefully where there will be regular changes in the
+                            activities of the class so as to interrupt the process of losing
+                            their attention (Graph 2). In this way, this will help students
+                            focus better and absorb more in class.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-6 mt-4">
+                    <div class="tour_card p-3">
+                        <img class="img-fluid w-100" src="<?php echo get_template_directory_uri(); ?>/inc/img/tour-005.jpg" alt="">
+                        <h5 class="py-2 fw-bold">Top Quality Notes</h5>
+                        <p>When Dr Aw created the worksheet, he will trial the
+                            questions in the shoes of students. In doing so, he is able to
+                            anticipate the conceptual challenges faced by the students.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-6 mt-4">
+                    <div class="tour_card p-3">
+                        <img class="img-fluid w-100" src="<?php echo get_template_directory_uri(); ?>/inc/img/tour-006.jpg" alt="">
+                        <h5 class="py-2 fw-bold">Concise and very easy to read Keywords Notes</h5>
+                        <p>The students are given top-notched teaching materials
+                            which contain frequently asked exam questions. It also
+                            contains Cambridge keywords.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-8 bg-purple p-4">
+                    <h2 class="text-light mb-3 fw-bold">Formative Assessment Close Monitoring of Students</h2>
+                    <h5 class="text-light fw-bold">As an award winning tutor, Dr Aw constantly check on
+                        the understanding of students. Dr Aw uses effective
+                        questioning techniques and provides feedback by
+                        marking the students work.</h5>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/tour-009.jpg" alt="">
+                </div>
+            </div>
+        </div>
     </div>
-
 </div>
 
 
